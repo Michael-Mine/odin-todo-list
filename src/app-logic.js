2b601e520b7520bac5680e51d8ef72dbea96a345
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2025-02-14 09:32:06 +0000

    Add other change functions & error msg in remove

--- src/app-logic.js
+++ src/app-logic.js
@@ -3,9 +3,9 @@ const itemList = [];
 
 let itemIndex = 0;
 
+//no methods to parse items to JSON for storage
 export function toDoItem (list, title, description, dueDate, priority, done, notes) {
     itemIndex += 1;
-    
     return { list, title, description, dueDate, priority, done, notes, itemIndex }
 }
 
@@ -19,35 +19,58 @@ const item3 = toDoItem("my list", "dishes", "cooking dinner", "Sunday", "medium"
 addItem(item2);
 addItem(item3);
 
-console.log(itemList);
+function findItem(item) {
+    let id = item.itemIndex;
+    let index = itemList.findIndex(element => element.itemIndex === id);
+    if (index !== -1) {
+        return element.itemIndex;
+    }
+};
 
+function changeTitle(item, newTitle) {
+    item.title = newTitle;
+};
 
-//rename function for title, description, notes
+function changeDescription(item, newDescription) {
+    item.description = newDescription;
+};
 
-//change list function
+function changeNotes(item, newNotes) {
+    item.notes = newNotes;
+};
+function changeList(item, newList) {
+    item.list = newList;
+};
 
-//change dueDate function - using api
-
-//change priority function
+function changePriority(item, newPriority) {
+    item.priority = newPriority;
+};
 
 function changeDone(item) {
-    item.done = "notDone" ? 
-    item.done = "done" : 
-    item.done = "notDone";
-}
-
-changeDone(item2);
+    item.done === "notDone" ? item.done = "done" : item.done = "notDone";
+};
 
 function removeItem(item) {
     let id = item.itemIndex;
     let index = itemList.findIndex(element => element.itemIndex === id);
     if (index !== -1) {
         itemList.splice(index, 1);
+    } else {
+        // change to alert for IU
+        console.log("error: item not found to remove");
     }
 };
 
+changeTitle(item2, "cook supper");
+changeDescription(item2, "cooking supper");
+changeNotes(item2, "30 mins");
+changeList(item2, "chores");
+changePriority(item2, "high");
+changeDone(item2);
 removeItem(item3);
 console.log(itemList);
+
+//change dueDate function - using api
 
 //add localStorage API - in separate module?
 
